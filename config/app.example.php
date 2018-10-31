fab35e7d96c5ca108a37eb6770825f22d9e86edb
Artem Kanunnykov <artemoldt@gmail.com>
2018-10-31 14:44:53 +0100

    config/app.example.php

--- config/app.example.php
+++ config/app.example.php
@@ -1,3 +1,15 @@
 <?php
 
-return [];
+return [
+    'AUTH_HMAC_SECRET' => 'kY73BD*l^&',
+
+    'MYSQL_PASSWORD' => getenv('MYSQL_PASSWORD'),
+    'MYSQL_USER' => getenv('MYSQL_USER'),
+    'MYSQL_DATABASE' => getenv('MYSQL_DATABASE'),
+
+    'VACATION_DAYS_COUNT' => 20,
+    'HOLIDAYS' => [
+        '2019-01-01',
+        '2019-01-07'
+    ]
+];
